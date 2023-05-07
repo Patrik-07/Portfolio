6f3e3710d34046d47624d30a6ab7751359b97a9b
Patrik Santa <patrik.santa07@gmail.com>
2023-05-07 23:34:48 +0200

    update

--- src/projects/projects.tsx
+++ src/projects/projects.tsx
@@ -1,20 +1,34 @@
-import { FigmaLogo } from "../logos/figma-logo";
-import { ProjectCard } from "./project-card";
+/* import { FigmaLogo } from "../logos/figma-logo";
+import { ProjectCard } from "./project-card"; */
 
 export const Projects = () => {
   return (
     <div className="md:px-16 pt-20">
-      <h1 className="text-3xl text-center dark:text-white pb-8">Projects</h1>
-      <div className="grid lg:grid-cols-2 grid-cols-1 lg:gap-14 gap-10 justify-items-center">
-        <ProjectCard title="SneakerShop" imgSrc="\assets\img\sneakerShop.png">
-          <FigmaLogo />
-          <FigmaLogo />
-        </ProjectCard>
-        <ProjectCard title="SneakerShop" imgSrc="\assets\img\sneakerShop.png">
-          <FigmaLogo />
-          <FigmaLogo />
-        </ProjectCard>
+      <h1 className="text-3xl text-center dark:text-white pb-4">Projects</h1>
+      <div className="text-2xl text-center dark:text-white pb-20">
+        <span>
+          Content upload in progress..., until then you can check out my
+          projects on{" "}
+        </span>
+        <u>
+          <a href="https://github.com/Patrik-07" target="_blank">
+            github.
+          </a>
+        </u>
       </div>
     </div>
   );
 };
+
+// <div className="grid lg:grid-cols-2 grid-cols-1 lg:gap-14 gap-10 justify-items-center">
+// </div>
+{
+  /* <ProjectCard title="SneakerShop" imgSrc="\assets\img\sneakerShop.png">
+<FigmaLogo />
+<FigmaLogo />
+</ProjectCard>
+<ProjectCard title="SneakerShop" imgSrc="\assets\img\sneakerShop.png">
+<FigmaLogo />
+<FigmaLogo />
+</ProjectCard> */
+}
